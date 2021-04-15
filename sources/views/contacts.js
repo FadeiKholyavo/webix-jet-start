6489c4b21237b9d73cb049577673e62fb78f0d1a
Fadei007 <fkholyavo@xbsoftware.by>
2021-04-15 22:29:54 +0300

    feat: create contacts data table

--- sources/views/contacts.js
+++ sources/views/contacts.js
@@ -1,3 +1,26 @@
-export default {
-	template:"Contacts page", css:"webix_shadow_medium app_start"
-};
+import {JetView} from "webix-jet";
+import {contacts} from "../models/contacts.js";
+
+export default class СontactsView extends JetView{
+	config(){
+		return {
+			cols:[
+				{					
+					view:"datatable",
+					scroll: "y",
+					columns: [
+						{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 50},
+						{id: "Name", header: "Name", width: 200},
+						{id: "Email", header: "Email", fillspace: true},
+						{id: "Status", header:{text: "Status", css:"datatable_header"}, css:"datatable_header", width: 100},
+						{id: "Country", header:{text: "Country", css:"datatable_header"}, css:"datatable_header", width: 100},
+					]	
+				},
+				{}
+			]
+		};
+	}
+	init(view){
+		view.queryView("datatable").parse(contacts);
+	}
+}
