916fa2c153f9b342857c557472f61f3294992369
Fadei007 <fkholyavo@xbsoftware.by>
2021-04-16 02:44:24 +0300

    refact: replace data table with list for contacts

--- sources/views/contacts.js
+++ sources/views/contacts.js
@@ -6,15 +6,12 @@ export default class СontactsView extends JetView{
 		return {
 			cols:[
 				{					
-					view:"datatable",
-					scroll: "y",
-					columns: [
-						{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 50},
-						{id: "Name", header: "Name", width: 200},
-						{id: "Email", header: "Email", fillspace: true},
-						{id: "Status", header:{text: "Status", css:"datatable_header"}, css:"datatable_header", width: 100},
-						{id: "Country", header:{text: "Country", css:"datatable_header"}, css:"datatable_header", width: 100},
-					]	
+					view:"list",
+					select:true,
+					template: "<span><b>#id#. #Name#</b><br>Email: #Email#,<br> Country: #Country# <br> Status: #Status#</span>",
+					type:{
+						height: "auto"
+					}
 				},
 				{
 					view: "form",
@@ -81,6 +78,6 @@ export default class СontactsView extends JetView{
 			};
 		}
 	init(view){
-		view.queryView("datatable").parse(contacts);
+		view.queryView("list").parse(contacts);
 	}
 }
