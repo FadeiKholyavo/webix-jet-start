6e4fa525b803252eda8e4621875fca86083c522f
Fadei007 <fkholyavo@xbsoftware.by>
2021-04-16 16:58:49 +0300

    refact: modify DatatableView class

--- sources/views/common.js
+++ sources/views/common.js
@@ -3,16 +3,137 @@ import {JetView} from "webix-jet";
 export default class CommonDatatableView extends JetView {
 	constructor(app, name, settings){
 		super(app, name); 
-		this.settings = settings;
+		this.tableSettings = settings;
+		this.formSettings = settings;
 	}
 	config(){
-		const datable = webix.extend({
+
+		const columns = this.tableSettings.columns;
+			
+		const datatable = webix.extend({
 			view:"datatable",
 			scroll: "y",
 			select: true,
+			localId: "datatable",
 			hover: "datatable-hover",
-		}, this.settings);
-		
-		return datable;
+			columns: columns,
+			onClick: {
+				"wxi-trash":(e, id) =>{
+					this.deleteItem(this.$$("datatable"),this.$$("form"), id);
+					return false;
+				}
+			}
+		}, this.tableSettings);
+
+		const elements = this.formSettings.fields;
+
+		elements.push({			
+			cols:[
+				{ 
+					view: "button", 
+					value: "Save",
+					css: "webix_primary",		
+					click: function(){
+						this.$scope.saveData(this.$scope.$$("datatable"),this.$scope.$$("form"));
+					}						
+				},
+				{ 
+					view: "button", 
+					value: "Clear",
+					click: function(){
+						this.$scope.clearForm(this.$scope.$$("form"));
+					}	
+				},
+				{ 
+					view: "button", 
+					value: "Unselect",
+					click: function(){
+						this.$scope.$$("datatable").unselectAll();
+					}
+				}
+			]
+		});
+		elements.push({});
+		const form = webix.extend({
+			view:"form",
+			localId: "form",
+			width: 300,
+			elements: elements,
+			rules: this.formSettings.rules
+			
+
+		}, this.formSettings);
+
+		return {cols:[
+			datatable,
+			form
+		]};
+	}
+	ready(){
+		this.$$("form").bind(this.$$("datatable"));
+	}
+	clearForm(form){
+		webix.confirm({
+			title: "Form cleaning",
+			text: "Do you realy want to clean up the form?"
+		}).then(
+			function(){
+				form.clear();
+				form.clearValidation();
+			}
+		);
+	}
+	saveData(table,form){
+
+		if(form.validate()){
+			
+			const formItem = form.getValues();
+			const formItemId = formItem.id;
+			
+			if(form.isDirty()){
+				//Protection against XSS
+				formItem.Name = webix.template.escape(formItem.Name);
+				formItem.Icon = formItem.Icon ? webix.template.escape(formItem.Icon): "";
+	
+				if(table.exists(formItemId)){
+	
+					form.save();
+	
+				}else{
+
+					form.save(formItem);		
+
+				}
+	
+				webix.message({
+					text: "Validation is succsessful",
+					type: "success",
+					expire: 1000
+				});
+			}else{
+				webix.message({
+					text: "You have not edited the data",
+					type: "info",
+					expire: 1000
+				});
+			}	  
+		}
+	}
+	deleteItem(table, form, tablelItemId){
+		webix.confirm({
+			title: "Country deleting",
+			text: "Do you really want to delete this country information"
+		}).then(
+			function(){
+				const formItemId = form.getValues().id;
+	
+				table.remove(tablelItemId);
+
+				if(formItemId == tablelItemId.row){
+					form.clear();
+				}
+			}
+		);
 	}
 }
+
--- sources/views/data.js
+++ sources/views/data.js
@@ -10,224 +10,73 @@ export default class DataView extends JetView{
 			cells: [
 				{
 					header: "Countries",
-					body:{
-						cols:[
-							new Datatable(this.app, "", {
-								id: "countriesDatatable",
-								columns: [
-									{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 70},
-									{id: "Name", header: "Name", fillspace:true},
-									{id:"delete", header: "", template:"{common.trashIcon()}", width: 60}
-								],
-								onClick: {
-									"wxi-trash":(e, id) =>{
-										this.deleteItem(webix.$$("countriesDatatable"), webix.$$("countriesForm"), id);
-										return false;
-									}
-								},
-								data: countries
-							}),
+					body:	new Datatable(this.app, "", {
+						columns:[
+							{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 70},
+							{id: "Name", header: "Name", fillspace:true},
+							{id:"delete", header: "", template:"{common.trashIcon()}", width: 60}
+						],
+						fields: [
 							{
-								view: "form",
-								id: "countriesForm",
-								width: 300,
-								elements:[
-									{
-										view: "template",
-										template: "edit country",
-										type: "section",
-										css: "section-font-size"
-									},
-									{ 
-										view: "text",
-										label: "Country",
-										name: "Name",
-										invalidMessage: "Enter the country name "
-									},
-									{
-										cols:[
-											{ 
-												view: "button", 
-												value: "Save",
-												css: "webix_primary",		
-												click: function(){
-													this.$scope.saveData(webix.$$("countriesDatatable"), webix.$$("countriesForm"));
-												}						
-											},
-											{ 
-												view: "button", 
-												value: "Clear",
-												click: function(){
-													this.$scope.clearForm(webix.$$("countriesForm"));
-												}	
-											},
-											{ 
-												view: "button", 
-												value: "Unselect",
-												click: function(){
-													webix.$$("countriesDatatable").unselectAll();
-												}
-											}
-										]
-									},
-									{}
-								],
-								rules:{
-									Name: function(value){
-										return webix.rules.isNotEmpty(value) && !webix.rules.isNumber(value);
-									},
-								}
+								view: "template",
+								template: "edit country",
+								type: "section",
+								css: "section-font-size"
+							},
+							{ 
+								view: "text",
+								label: "Country",
+								name: "Name",
+								invalidMessage: "Enter the country name "
+							},
+						],
+						rules:{
+							Name: function(value){
+								return webix.rules.isNotEmpty(value) && !webix.rules.isNumber(value);
 							}
-						]
+						},
+						data: countries
 					}
+					),
 				},
 				{
 					header: "Statuses",
-					body: {
-						cols:[
-							new Datatable(this.app, "", {
-								id: "statusesDatatable",
-								columns: [
-									{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 70},
-									{id: "Name", header: "Name", fillspace:true},
-									{id: "Icon", header:{text: "Icon", css:"datatable_header"}, css:"datatable_header", width: 100},
-									{id:"delete", header: "", template:"{common.trashIcon()}", width: 60}
-								],
-								onClick: {
-									"wxi-trash":(e, id) =>{
-										this.deleteItem(webix.$$("statusesDatatable"), webix.$$("statusesForm"), id);
-										return false;
-									}
-								},	
-								data: statuses
-							}),
-							{	
-								view: "form",
-								id: "statusesForm",
-								width: 300,
-								elements:[
-									{
-										view: "template",
-										template: "edit status",
-										type: "section",
-										css: "section-font-size"
-									},
-									{ 
-										view: "text",
-										label: "Status",
-										name: "Name",
-										invalidMessage: "Enter the status name"
-									},
-									{ 
-										view: "text",
-										label: "Icon",
-										name: "Icon",
-										invalidMessage: "Enter the icon name"
-									},
-									{
-										cols:[
-											{ 
-												view: "button", 
-												value: "Save",
-												css: "webix_primary",			
-												click: function(){
-													this.$scope.saveData(webix.$$("statusesDatatable"), webix.$$("statusesForm"));
-												}					
-											},
-											{ 
-												view: "button", 
-												value: "Clear",
-												click: function(){
-													this.$scope.clearForm(webix.$$("statusesForm"));
-												}		
-											},
-											{ 
-												view: "button", 
-												value: "Unselect",
-												click: function(){
-													webix.$$("statusesDatatable").unselectAll();
-												}	
-											}
-										]
-									},
-									{}
-								],
-								rules:{
-									Name: webix.rules.isNotEmpty,
-									Icon: webix.rules.isNotEmpty
-								}
-							}
-						]
+					body:	new Datatable(this.app, "", {	
+						columns:[
+							{id: "id", header: {text: "id", css:"datatable_header"}, css:"datatable_header", width: 70},
+							{id: "Name", header: "Name", fillspace:true},
+							{id: "Icon", header:{text: "Icon", css:"datatable_header"}, css:"datatable_header", width: 100},
+							{id:"delete", header: "", template:"{common.trashIcon()}", width: 60}
+						],
+						fields: [
+							{
+								view: "template",
+								template: "edit status",
+								type: "section",
+								css: "section-font-size"
+							},
+							{ 
+								view: "text",
+								label: "Status",
+								name: "Name",
+								invalidMessage: "Enter the status name"
+							},
+							{ 
+								view: "text",
+								label: "Icon",
+								name: "Icon",
+								invalidMessage: "Enter the icon name"
+							},
+						],
+						rules:{
+							Name: webix.rules.isNotEmpty,
+							Icon: webix.rules.isNotEmpty
+						},
+						data: statuses
 					}
+					),
 				},
 			]
 		};
-	}
-	ready(){
-		webix.$$("countriesForm").bind(webix.$$("countriesDatatable"));
-		webix.$$("statusesForm").bind(webix.$$("statusesDatatable"));
-	}	
-	clearForm(form){
-		webix.confirm({
-			title: "Form cleaning",
-			text: "Do you realy want to clean up the form?"
-		}).then(
-			function(){
-				form.clear();
-				form.clearValidation();
-			}
-		);
-	}
-	saveData(table,form){
-
-		if(form.validate()){
-			
-			const formItem = form.getValues();
-			const formItemId = formItem.id;
-			
-			if(form.isDirty()){
-				//Protection against XSS
-				formItem.Name = webix.template.escape(formItem.Name);
-				formItem.Icon = formItem.Icon ? webix.template.escape(formItem.Icon): "";
-	
-				if(table.exists(formItemId)){
-	
-					form.save();
-	
-				}else{
-
-					form.save(formItem);		
-
-				}
-	
-				webix.message({
-					text: "Validation is succsessful",
-					type: "success",
-					expire: 1000
-				});
-			}else{
-				webix.message({
-					text: "You have not edited the data",
-					type: "info",
-					expire: 1000
-				});
-			}	  
-		}
-	}
-	deleteItem(table, form, tablelItemId){
-		webix.confirm({
-			title: "Country deleting",
-			text: "Do you really want to delete this country information"
-		}).then(
-			function(){
-				const formItemId = form.getValues().id;
-	
-				table.remove(tablelItemId);
-
-				if(formItemId == tablelItemId.row){
-					form.clear();
-				}
-			}
-		);
 	}
 }
