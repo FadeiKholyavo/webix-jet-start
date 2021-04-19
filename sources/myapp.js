5003fdb39e8d4e8db96637dded3343fa96f3c484
Fadei007 <fkholyavo@xbsoftware.by>
2021-04-19 15:22:06 +0300

    feat: create helper function

--- sources/myapp.js
+++ sources/myapp.js
@@ -1,5 +1,6 @@
 import "./styles/app.css";
-import {JetApp, EmptyRouter, HashRouter } from "webix-jet";
+import {JetApp, EmptyRouter, HashRouter, plugins } from "webix-jet";
+import State from "./helpers/state.js"
 
 export default class MyApp extends JetApp{
 	constructor(config){
@@ -16,5 +17,13 @@ export default class MyApp extends JetApp{
 }
 
 if (!BUILD_AS_MODULE){
-	webix.ready(() => new MyApp().render() );
+	if (!BUILD_AS_MODULE){
+		webix.ready(() => {
+			const app = new MyApp();
+			app.use(plugins.Locale);
+			app.setService("state", new State(app));
+			app.render();
+		
+		});
+	}
 }
